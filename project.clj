bbef8807035168808e4f10e1a771e90985ccfb93
Sarah Roberts <sarahr@cyverse.org>
2018-02-19 17:21:55 -0700

    updated the dependencies

--- project.clj
+++ project.clj
@@ -20,9 +20,13 @@
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :manifest {"Git-Ref" ~(git-ref)}
   :uberjar-name "sharkbait-standalone.jar"
-  :plugins [[jonase/eastwood "0.2.3"]
-            [test2junit "1.2.2"]]
-  :dependencies [[edu.internet2.middleware.grouper/grouper "2.3.0"]
+  :plugins [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+            [test2junit "1.2.2" :exclusions [org.clojure/clojure]]]
+  :dependencies [[edu.internet2.middleware.grouper/grouper "2.3.0"
+                  :exclusions [javax.transaction/jta
+                               org.wso2.charon/org.wso2.charon.core
+                               org.wso2.charon/org.wso2.charon.samples]]
+                 [javax.transaction/jta "1.1"]
                  [honeysql "0.6.2"]
                  [korma "0.4.2"]
                  [net.sf.ehcache/ehcache "2.10.1"]
